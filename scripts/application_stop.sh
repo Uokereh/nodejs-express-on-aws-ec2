f83a2b27ae951b20cc380f541afce94fb4fad301
Uokereh <Uokereh@users.noreply.github.com>
2021-08-23 22:54:04 -0400

    comit

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,7 +2,7 @@
 #Stopping existing node servers
 echo "Stopping any existing node servers"
 APP_ID=$(ps -C node --no-header --format 'pid')
-
+echo "APP_ID is $APP_ID"
 if [ -n "${APP_ID}" ]; then
     echo "Stopping instance $APP_ID"
     kill -9 $APP_ID
